052893898597cb9550d2a27270aa3a1fa64f1592
Sangwon Song <mrsong0822@gmail.com>
2017-10-03 16:56:14 -0400

    WIP still

--- assets/javascript/movies.js
+++ assets/javascript/movies.js
@@ -1,13 +1,17 @@
 var movies_api = "2B2888AB-A2D4-437C-A0BD-5C58BF0211E3\n";
-var user_search = "";
-var queryURL_v1 = "https://api.amctheatres.com/v1/theatres/42\n";
-var queryURL_v2 = "https://api.amctheatres.com/v2/theatres/42\n";
+//var user_search = "";
+//var queryURL_v1 = "https://api.amctheatres.com/v1/";
+var queryURL_v2 = "https://api.amctheatres.com/v2/";
 var state = "georgia";
 
+var georgia_queryURL = queryURL_v2 + "locations/states/" + state;
 
-.ajax({
+
+$.ajax({
+    url: georgia_queryURL,
     headers: {'X-AMC-Vendor-Key': movies_api},
     method: 'GET'
 }).done(function(movieData){
+    console.log(movieData);
 
 });
