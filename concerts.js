a500459ce9c54b355aabb03dca370832e910d9dd
M4trix404 <M4trix404@users.noreply.github.com>
2017-10-02 21:33:13 -0400

    Add files via upload

---
+++ concerts.js
@@ -0,0 +1,27 @@
+
+var concerts_apikey= "qqlfe7PARIytlsCOVfsRaER3zT5toMOq";
+var user_search= "concerts";
+var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + concerts_apikey +"&radius=30&unit=miles&city=atlanta&classificationName=music&count=10";
+
+$.ajax({
+    type:"GET",
+    url: queryURL,
+    async:true,
+    dataType: "json",
+    success: function(json) {
+        console.log(json);
+        // Parse the response.
+        // Do other things.
+    },
+    error: function(xhr, status, err) {
+        // This time, we do not end up here!
+    }
+});
+// $.ajax({
+//     url: queryURL,
+//     headers:  {'user-key': restaurants_api }, //different from class's ajax call, we need to add
+//     method: 'GET'
+// }).done(function(concertsData){
+//     console.log(queryURL);
+//     restaurantData
+// });
