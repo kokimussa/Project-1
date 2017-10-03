0aff14b42ce1b2388da50366954a0aa00d714bc3
Sangwon Song <ssong86@gatech.edu>
2017-10-03 19:20:45 -0400

    WIP

--- assets/javascript/movies.js
+++ assets/javascript/movies.js
@@ -1,4 +1,4 @@
-var movies_api = "2B2888AB-A2D4-437C-A0BD-5C58BF0211E3\n";
+var movies_api = "2B2888AB-A2D4-437C-A0BD-5C58BF0211E3";
 //var user_search = "";
 //var queryURL_v1 = "https://api.amctheatres.com/v1/";
 var queryURL_v2 = "https://api.amctheatres.com/v2/";
@@ -9,8 +9,12 @@ var georgia_queryURL = queryURL_v2 + "locations/states/" + state;
 
 $.ajax({
     url: georgia_queryURL,
-    headers: {'X-AMC-Vendor-Key': movies_api},
-    method: 'GET'
+    beforeSend: function(request){
+        request.setRequestHeader('X-AMC-Vendor-Key', movies_api)
+    },
+    method: 'GET',
+    processData: false,
+    contentType: false
 }).done(function(movieData){
     console.log(movieData);
 
