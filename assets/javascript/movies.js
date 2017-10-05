bd3efeb188bb656d377539933ca55726862788df
Sangwon Song <ssong86@gatech.edu>
2017-10-05 00:36:20 -0400

    WIP

--- assets/javascript/movies.js
+++ assets/javascript/movies.js
@@ -6,4 +6,11 @@ $.ajax({
     method: 'GET'
 }).done(function(movieData){
   console.log(movieData);
+  var movieResult = movieData.results;
+  for(var i = 0; i < movieResult.length; i++){
+
+  }
+
 })
+
+https://api.themoviedb.org/3/movie/now_playing?api_key=616f4815b47dbe302165b78598598d02&language=en-US&page=1/9E2y5Q7WlCVNEhP5GiVTjhEhx1o.jpg
